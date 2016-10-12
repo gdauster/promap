9a11e100f88904ed1fd1aa58ba2d056e9a9fa7d2
Guillaume Dauster <guillaume.dauster@gmail.com>
2016-10-12 16:18:01 +0200

    work on other ip adress

--- src/main.js
+++ src/main.js
@@ -1,46 +1,74 @@
 class Deformation {
   constructor() {
     // ATTRIBUTES
-    this.geometry = new THREE.PlaneGeometry(10, 5);
-    this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    this.DtoR = Math.PI / 180.0;
+    this.geometry = new THREE.PlaneGeometry(5, 4);
+    this.material = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+    this.material2 = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+    this.main = new THREE.Object3D();
+    this.window = new THREE.Mesh(this.geometry, this.material);
+    this.pivot = new THREE.Object3D();
+    this.rotation = new THREE.CircleGeometry(0.5, 16);
+    this.rotation.vertices.shift();
+    this.pivotXaxis = new THREE.Line(
+      this.rotation,
+      new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+      this.pivotYaxis = new THREE.Line(
+        this.rotation,
+        new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+        this.pivotZaxis = new THREE.Line(
+          this.rotation,
+          new THREE.MeshBasicMaterial({ color: 0x0000ff }));
+    this.pivotXaxis.rotateOnAxis(new THREE.Vector3(1, 0, 0), this.DtoR * 90);
+this.pivotZaxis.rotateOnAxis(new THREE.Vector3(0, 1, 0), this.DtoR * 90);
+    this.pivot.add(this.window, this.pivotXaxis, this.pivotYaxis, this.pivotZaxis);
+    this.main.add(this.pivot);
+  }
+  movePivot(position) {
+    this.pivot.position.copy(position);
+    this.window.position.copy(position);
+    this.window.position.multiplyScalar(-1);
   }
 }
 
 class ProMap {
   constructor(containerId) {
+    // ATTRIBUTES
     this.container = document.getElementById(containerId);
     this.callbacks = {};
-
     this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75, this.container.offsetWidth / this.container.offsetHeight, 0.1, 1000);
+    this.controls = new THREE.TrackballControls( this.camera );
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: false,
+      preserveDrawingBuffer: true,
+    });
+    this.axisHelper = new THREE.AxisHelper(15);
+    this.deformation = new Deformation();
 
-    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    // Initialise 3D elements
     this.camera.position.z = 5;
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    this.controls = new THREE.TrackballControls( this.camera );
-    this.controls.rotateSpeed = 1.0;
+    this.controls.rotateSpeed = 2.0;
     this.controls.zoomSpeed = 1.2;
     this.controls.panSpeed = 0.8;
     this.controls.noZoom = false;
     this.controls.noPan = false;
     this.controls.staticMoving = false;
-    this.controls.dynamicDampingFactor = 0.3;
+    this.controls.dynamicDampingFactor = 0.9;
     this.controls.keys = [ 65, 83, 68 ];
 
-    const opts = {
-      antialias: true,
-      alpha: false,
-      preserveDrawingBuffer: true,
-    };
-    this.renderer = new THREE.WebGLRenderer(opts);
-    this.renderer.setSize( this.container.offsetWidth, this.container.offsetHeight );
+    this.renderer.setSize(this.container.offsetWidth,
+                          this.container.offsetHeight);
     this.renderer.setClearColor(0xffffff);
 
-    const axisHelper = new THREE.AxisHelper(15);
-    this.scene.add( axisHelper );
-
     this.container.appendChild(this.renderer.domElement);
+
+    // Add elements to the scene and attach events
+    this.scene.add(this.axisHelper, this.deformation.main);
     this.attach(['resize']);
   }
   attach(events) {
